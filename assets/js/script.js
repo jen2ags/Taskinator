e6858e4e1bf0dfd49f6be6444673dfed1038b47a
jennj <jennjenn93@gmail.com>
2022-02-23 18:50:01 -0600

    fixing submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 
+
 var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -18,14 +19,44 @@ var taskFormHandler = function(event) {
     document.querySelector("input[name='task-name']").value = "";
     document.querySelector("select[name='task-type']").selectedIndex = 0;
 
+    var isEdit = formEl.hasAttribute("data-task-id");
+
 
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
-    //send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);
+    
+    //has data attribute, so get task id and call function to complete edit
+    if (isEdit) {
+      var taskId = formEl.getAttribute("data-task-id");
+      completeEditTask(taskNameInput,taskTypeInput, taskId);
+    }
+
+    //no data attribute, so create object as normal
+    else {
+      var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+      };
+
+      createTaskEl(taskDataObj);
+    }
+};
+
+var completeEditTask = function(taskName, taskType, taskId){
+  //find the matching task list item
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+//set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
 };
 
 var createTaskEl = function(taskDataObj) {
